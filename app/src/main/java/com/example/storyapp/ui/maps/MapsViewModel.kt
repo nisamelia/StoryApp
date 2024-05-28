ff872ecce29139709604a392911a947068eeef1b
nisamelia <amelianisa16@gmail.com>
2024-05-28 08:56:40 +0700

    add geo loc + maps

---
+++ app/src/main/java/com/example/storyapp/ui/maps/MapsViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.storyapp.ui.maps
+
+import android.net.http.HttpException
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.storyapp.data.pref.StoryRepository
+import com.example.storyapp.data.response.StoryResponse
+import kotlinx.coroutines.launch
+
+class MapsViewModel(private val repository: StoryRepository) : ViewModel() {
+    private val _story = MutableLiveData<StoryResponse>()
+    val story: LiveData<StoryResponse> = _story
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    fun setMarkers() {
+        _isLoading.value = true
+        viewModelScope.launch {
+            try {
+                val mapResponse = repository.setMap()
+                _story.value = mapResponse
+            } catch (e: retrofit2.HttpException) {
+                _story.value = StoryResponse(emptyList(), error = true, message = e.message)
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+}
